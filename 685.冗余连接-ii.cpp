8abfd6956925a0ddebcfa40bd18e75142ace0d68
lzh-g <72566778+lzh-g@users.noreply.github.com>
2024-10-28 23:52:51 +0800

    [lzh]685update

--- 685.冗余连接-ii.cpp
+++ 685.冗余连接-ii.cpp
@@ -8,69 +8,79 @@
 
 using namespace std;
 // @lc code=start
+
 class Solution
 {
 public:
-    void init(vector<int> &ancestor)
+    vector<int> ancestor;
+
+    void init(int n)
     {
-        for (int i = 0; i < ancestor.size(); i++)
+        ancestor.resize(n);
+        for (int i = 0; i < n; ++i)
         {
             ancestor[i] = i;
         }
     }
 
-    int find(vector<int> &ancestor, int node)
+    int find(int index)
     {
-        return ancestor[node] == node ? node : ancestor[node] = find(ancestor, ancestor[node]);
+        return index == ancestor[index] ? index : ancestor[index] = find(ancestor[index]);
     }
 
-    void Merge(vector<int> &ancestor, int node1, int node2)
+    void merge(int u, int v)
     {
-        ancestor[find(ancestor, node1)] = find(ancestor, node2);
+        ancestor[find(u)] = find(v);
     }
     vector<int> findRedundantDirectedConnection(vector<vector<int>> &edges)
     {
         int n = edges.size();
-        vector<int> ancestor(n + 1);
-        init(ancestor);
-
-        int confilt = -1;
+        init(n + 1);
+        auto parent = vector<int>(n + 1);
+        for (int i = 1; i <= n; ++i)
+        {
+            parent[i] = i;
+        }
+        int conflict = -1;
         int cycle = -1;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; ++i)
         {
             auto edge = edges[i];
             int node1 = edge[0], node2 = edge[1];
-            if (ancestor[node2] != node2)
+            if (parent[node2] != node2)
             {
-                confilt = i;
+                conflict = i;
             }
             else
             {
-                ancestor[node2] = node1;
-                if (find(ancestor, node1) == find(ancestor, node2))
+                parent[node2] = node1;
+                if (find(node1) == find(node2))
                 {
                     cycle = i;
                 }
                 else
                 {
-                    Merge(ancestor, node1, node2);
+                    merge(node1, node2);
                 }
             }
         }
-        if (confilt < 0)
+        if (conflict < 0)
         {
-            return vector<int>{edges[cycle][0], edges[cycle][1]};
+            auto redundant = vector<int>{edges[cycle][0], edges[cycle][1]};
+            return redundant;
         }
         else
         {
-            auto confiltEdge = edges[confilt];
+            auto conflictEdge = edges[conflict];
             if (cycle >= 0)
             {
-                return vector<int>{ancestor[confiltEdge[1]], confiltEdge[1]};
+                auto redundant = vector<int>{parent[conflictEdge[1]], conflictEdge[1]};
+                return redundant;
             }
             else
             {
-                return vector<int>{confiltEdge[0], confiltEdge[1]};
+                auto redundant = vector<int>{conflictEdge[0], conflictEdge[1]};
+                return redundant;
             }
         }
     }
