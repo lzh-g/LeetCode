73d515027d78f24a6b202f0a8059977e76461fd9
lzh-g <72566778+lzh-g@users.noreply.github.com>
2024-11-17 23:17:42 +0800

    [1] 哈希表

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -3,13 +3,29 @@
  *
  * [1] 两数之和
  */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = mp.find(target - nums[i]);
+            if (it != mp.end())
+            {
+                return {it->second, i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
     }
 };
 // @lc code=end
-
