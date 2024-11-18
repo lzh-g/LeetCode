3ad825bc92f1fd5dcc53e43552a60884c1bc6178
lzh-g <72566778+lzh-g@users.noreply.github.com>
2024-11-18 23:49:55 +0800

    [49] 哈希计数匹配

--- 49.字母异位词分组.cpp
+++ 49.字母异位词分组.cpp
@@ -14,7 +14,6 @@ class Solution
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-
         // 思路：统计每个字符串的字母数,mp的key-value结构为
         // "a1b2...z3":"str"，即key为各字母出现的次数，value为对应这种情况的所有字符串
         // 完成后，把mp转换成输出对应的格式
